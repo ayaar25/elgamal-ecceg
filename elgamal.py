cc83d979d85a7d5077650d14d2bca57c7d8c1bbe
Aya Aurora <ayyya.aurora@gmail.com>
2019-03-25 12:27:30 +0700

    add format message

--- elgamal.py
+++ elgamal.py
@@ -1,6 +1,7 @@
 import random
 import pickle
 import array
+import re
 from base.general import is_prime
 
 class ElGamal(object):
@@ -23,11 +24,9 @@ class ElGamal(object):
   def write_file(self, filename, data):
     with open(filename, 'wb') as file:
       file.write(data)
-      # pickle.dump(data, file)
-  
+      
   def write_file_list(self, filename, data):
     with open(filename, 'wb') as file:
-      # file.write(data)
       pickle.dump(data, file)
 
   def key_gen(self, p):
@@ -36,13 +35,9 @@ class ElGamal(object):
     g = random.randrange(p)
     x = random.randrange(1,p - 2)
     y = (g**x)%p
-    # key_public = ''+str(y)+','+str(g)+','+str(p)
     key_public = [y,g,p]
-    # key_public = bytes(key_public, "utf-8")
-    # key_private = ''+str(x)+','+str(p)
     key_private = [x,p]
-    # key_private = bytes(key_private, "utf-8")
-
+    
     self.write_file_list("key.pub", key_public)
     self.write_file_list("key.pri", key_private)
   
@@ -67,13 +62,13 @@ class ElGamal(object):
       list_cipher_int.append(a)
       list_cipher_int.append(b)
     print(list_plain_int)
-    self.write_file_list("cipher.txt", list_cipher_int)
+    regex = re.compile('.\w+').findall(filename)
+    print(regex)
+    self.write_file_list("cipher" + regex[-1], list_cipher_int)
     print(list_cipher_int)
   
   def decrypt(self, filename, key_public, key_private):
     list_cipher_int = self.read_file_list(filename)
-    # list_ciphertext = list(ciphertext)
-    # list_cipher_int = list(map(int, ciphertext))
     key_pub = self.read_file_list(key_public)
     key_pri = self.read_file_list(key_private)
 
@@ -82,19 +77,17 @@ class ElGamal(object):
     p = key_pub[2]
     x = key_pri[0]
     
-    # print("y: ", y, "g: ", g, "p: ", p, "x: ", x)
     list_plain_int = []
     inc = 0
     while inc < len(list_cipher_int):
       pangkat = p-1-x
       axinf = (list_cipher_int[inc]**pangkat)%p
-      # print(inc, pangkat, axinf)
       message = (axinf*list_cipher_int[inc+1])%p
       list_plain_int.append(message)
       inc+=2
 
-    
-    self.write_file("out.txt", array.array('B', list_plain_int).tobytes())
+    regex = re.compile('.\w+').findall(filename)
+    self.write_file("out"+ regex[-1], array.array('B', list_plain_int).tobytes())
     print(list_plain_int)
 
 if __name__ == '__main__':
