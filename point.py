beca35f2f904ec3d111e5698c2c98f66afd3aa8e
Aya Aurora <ayyya.aurora@gmail.com>
2019-03-25 12:52:01 +0700

    add point class

---
+++ point.py
@@ -0,0 +1,20 @@
+
+class Point(object):
+  
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+  
+  def _get_x(self):
+    return self.x
+  
+  def _get_y(self):
+    return self.y
+  
+  def _set_x(self, x):
+    self.x = x
+  
+  def _set_y(self, y):
+    self.y = y
+  
+  
