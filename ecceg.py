2aaa4a9c7e5b858373bb3a3b2101e752c73eed45
Aya Aurora <ayyya.aurora@gmail.com>
2019-03-25 18:18:46 +0700

    create generate field

--- ecceg.py
+++ ecceg.py
@@ -3,6 +3,7 @@ import pickle
 import array
 import re
 from base.general import is_prime
+from base.point import Point
 
 class EllipticCurveCryptoElGamal(object):
 
@@ -13,6 +14,15 @@ class EllipticCurveCryptoElGamal(object):
     self.P = P
     pass
   
+  def _get_A(self):
+    return self.A
+  
+  def _get_B(self):
+    return self.B
+  
+  def _get_P(self):
+    return self.P
+
   def read_file(self, filename):
     data = None
     with open(filename, 'rb') as file:
@@ -32,9 +42,34 @@ class EllipticCurveCryptoElGamal(object):
   def write_file_list(self, filename, data):
     with open(filename, 'wb') as file:
       pickle.dump(data, file)
-    
-  def compute_ecc_equation(self, k, point):
-    pass
+
+  def generate_pow_two(self):
+    list_powered_two = []
+    for i in range(self._get_P()):
+      list_powered_two.append(i*i)
+    return list_powered_two
+
+  def generate_galois_field(self):
+    field = []
+    for i in range(self._get_P()):
+      print(i)
+      list_y = self.compute_ecc_equation(i)
+      if list_y:
+        for j in range(len(list_y)):
+          point = Point()
+          point._set_x(i)
+          point._set_y(list_y[j])
+          field.append(point)
+    return field
+  
+  def compute_ecc_equation(self, x):
+    result = []
+    list_powered_two = self.generate_pow_two()
+    y = x**3 + self._get_A()*x + self._get_B()
+    for i in range(self._get_P()):
+      if list_powered_two[i] % self._get_P() == y % self._get_P():
+        result.append(i)
+    return result
     
   def encoding(self, list_message):
     pass
@@ -47,3 +82,10 @@ class EllipticCurveCryptoElGamal(object):
 
   def decrypt(self, filename, key_public, key_private):
     pass
+
+if __name__ == '__main__':
+  ecceg = EllipticCurveCryptoElGamal(1,12,11)
+  field = ecceg.generate_galois_field()
+  for i in range(len(field)):
+    print(field[i]._get_x(),field[i]._get_y())
+  # print(ecceg.generate_pow_two())
