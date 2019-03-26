4062432187f5ad41816e798b00a5df6a5df40955
Aya Aurora <ayyya.aurora@gmail.com>
2019-03-26 14:54:00 +0700

    add encoding

--- ecceg.py
+++ ecceg.py
@@ -7,13 +7,13 @@ from point import Point
 
 class EllipticCurveCryptoElGamal(object):
 
-  def __init__(self, A, B, P):
+  def __init__(self, A, B, P, k):
     # ECC Equation Constanta, y^2 mod P = x^3 + Ax + B mod P
     self.A = A
     self.B = B
     self.P = P
-    pass
-  
+    self.k = k # For encoding & decoding
+    
   def _get_A(self):
     return self.A
   
@@ -22,6 +22,9 @@ class EllipticCurveCryptoElGamal(object):
   
   def _get_P(self):
     return self.P
+  
+  def _get_k(self):
+    return self.k
 
   def read_file(self, filename):
     data = None
@@ -52,7 +55,7 @@ class EllipticCurveCryptoElGamal(object):
   def generate_galois_field(self):
     field = []
     for i in range(self._get_P()):
-      print(i)
+      # print(i)
       list_y = self.compute_ecc_equation(i)
       if list_y:
         for j in range(len(list_y)):
@@ -72,20 +75,42 @@ class EllipticCurveCryptoElGamal(object):
     return result
     
   def encoding(self, list_message):
-    pass
+    list_message_int = list(map(int, list_message))
+    # print("lent massage: ", len(list_message_int))
+    result = []
+    for i in range(len(list_message_int)):
+      for j in range(self._get_k()-1):
+        x = (list_message_int[i] * self._get_k() + (j + 1))
+        y = self.compute_ecc_equation(x)
+        if y:
+          point = Point()
+          point._set_x(x)
+          point._set_y(y[0]) 
+          result.append(point)
+    return result
 
   def decoding(self, list_message):
     pass
   
-  def encrypt(self, filename, key_public, key_private):
-    pass
+  def encrypt(self, filename):
+    plaintext = self.read_file(filename)
+    list_plaintext = list(plaintext)
+    list_plain_int = list(map(int, plaintext))
+  
+    return self.encoding(list_plain_int)
 
   def decrypt(self, filename, key_public, key_private):
     pass
 
 if __name__ == '__main__':
-  ecceg = EllipticCurveCryptoElGamal(1,12,11)
+  ecceg = EllipticCurveCryptoElGamal(-1,188,751,5)
   field = ecceg.generate_galois_field()
-  for i in range(len(field)):
-    print(field[i]._get_x(),field[i]._get_y())
+  result = ecceg.encrypt("plain.txt")
+  for i in range(len(result)):
+    print(result[i]._get_x(),result[i]._get_y())
+  # p = Point()
+  # p._set_x(2)
+  # p._set_y(4)
+  # if p in field:
+  #   print('Ade brur')
   # print(ecceg.generate_pow_two())
